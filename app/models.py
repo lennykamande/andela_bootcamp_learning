5011e62209d8b1cf52b39374c84485f11d1413e0
lennykamande <lennymanyeki@gmail.com>
2017-09-27 14:54:00 +0300

    New changes

--- app/models.py
+++ app/models.py
@@ -1,37 +1,64 @@
-
-users = []
+#Class user to handle signup and login and register
 class User(object):
 
-    def __init__(self, firstname, lastname, username, email, password):
-    	self.username = username
-    	self.email = email	
-    	self.password = password
-    	self.firstname = firstname
-    	self.lastname = lastname
+
+    """a list that holds all users"""
+    userlist =[]
+
+    def __init__(self):
+        self.details={}
+
+
+    def signup(self,username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+        for user in self.userlist:
+            if username == user['username'] or email == user['email']:
+                return "username or email exists"
+        else:
+            self.details ={'username' : self.username, 'email' : self.email , 'password' :  self.password}
+            self.userlist.append(self.details)
+            print(self.userlist)
+            return "registered"
+
+
+    def login(self,username, password):
+        for user in self.userlist:
+            if user['username'] == username:
+                print(user)
+                if user['password'] == password:
+                    return True
+        else:
+            return False
 		
-    def register(self, username, firstname, lastname, email, password):
-			x = users.append(username, firstname, password, email, lastname )
-			print x
-    def login(self, email, password):
-			return next((obj for obj in users if obj.email == email and obj.password == password), none)
-
-
-    def logout(User):
-		"""Logout user from sessions."""
-			pass
-	@property
-    def is_authenticated(self):
-        """Return True if the user is authenticated."""
-        return self.authenticate
-
 class Shoppinglist(object):
-	
-    def __init__(self, title, descripition, date, user_id):
-    	self.title = title
-    	self.descripition = descripition	
-    	self.date = datetime.now().strftime('%Y-%m-%d')
-    	self.shopping =[]
+    """a class to manage a shopping list"""
 
-    def create(self, title, descripition, date):
-    	self.shopping.append('')
+    #a list to hold all shopping lists
+    shoppinglists = []
 
+    def __init__(self ):
+        """ initialize a dictionary to hold the details of a particular list"""
+        self.listsdict = {}
+
+    def newlist(self, listname, owner, listid):
+        """ A method to create a new list"""
+        for shoplist in self.shoppinglists:
+            if shoplist['listname'] == listname and shoplist['owner'] == owner:
+                return 'list exists'
+        # create the list if there is no list with the provided details
+        else:
+            self.listsdict = {'listname' : listname, 'owner' : owner, 'listid' : listid}
+            self.shoppinglists.append(self.listsdict)
+            res = 'success'
+            return res
+
+    def view_lists(self, owner):
+        """ a method to show lists beonging to a particular user"""
+        #a new list containing names of lists belonging to  a certain user
+        user_shoplist = []
+        for s_list in self.shoppinglists:
+            if s_list['owner']== owner:
+                user_shoplist.append(s_list)
+        return user_shoplist
--- app/views.py
+++ app/views.py
@@ -1,41 +1,41 @@
-from flask import render_template, url_for, request, redirect, session
-
+import os
+from flask import Flask, url_for,render_template, request, flash, redirect, session
 from app import app
 from models import User, Shoppinglist
-
-
-
-@app.route('/dashboard')
-def dashboard():
-    
-    return render_template("dashboard.html")
 
 @app.route('/', methods=['GET','POST'])
 def login():
     if request.method=='POST':
-        email=request.form['email']
-        password=request.form['password']
-        check = User(email,password)
-        check.login
-        if (check.login):
+        username = request.form['username']
+        password = request.form['password']
+        res = User.login(username=username, password=password)
+        print(res)
+        if res == True:
             return redirect(url_for('dashboard'))
     else:
-       return render_template("login.html")
-    
+        
+        return render_template('login.html')
 
-@app.route('/register',  methods=['GET', 'POST'])
+@app.route('/dashboard')
+def dashboard_view():
+    """route to render dashboard page"""
+    return render_template('dashboard.html')
+
+@app.route('/register', methods=['GET','POST'])
 def register():
     if request.method=='POST':
-        firstname=request.form['fname']
-        lastname=request.form['lname']
-        username=request.form['username']
-        emailid=request.form['email']
-        password = request.form['pass']
-        cpassword = request.form['passc']
-        if (password == cpassword):
-           new_user = User(firstname,lastname, username, emailid, password) 
-           new_user.register
-           return redirect(url_for('login'))
-    else:
-        return render_template("register.html")
+        username = request.form['username']
+        email = request.form['email']
+    # counter check if password is the same
+        password = request.form['password']
+        confpassword = request.form['confpassword']
 
+        if password != confpassword:
+            
+            return redirect(url_for('register'))
+        res = User.signup(username, email, password)
+        if res == "username or email exists":
+            flash('username or email already taken', 'alert-danger')
+            return redirect(url_for('signup'))
+    else:    
+        return render_template('register.html')
