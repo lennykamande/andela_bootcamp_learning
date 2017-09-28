15140e82c5829ee9da55b63010d64bf993d2262a
lennykamande <lennymanyeki@gmail.com>
2017-09-28 14:22:43 +0300

    Added a List item

--- app/models.py
+++ app/models.py
@@ -1,9 +1,8 @@
 #Class user to handle signup and login and register
+from random import *
 class User(object):
-
-
-    """a list that holds all users"""
-    userlist =[]
+   
+    userlist = []
 
     def __init__(self):
         self.details={}
@@ -23,42 +22,72 @@ class User(object):
             return "registered"
 
 
-    def login(self,username, password):
+    def login(self,email, password):
+        self.email = email
+        self.password = password
         for user in self.userlist:
-            if user['username'] == username:
-                print(user)
+            if email == user['email']:
+                print (user)
                 if user['password'] == password:
                     return True
         else:
             return False
-		
+shoppinglists = []		
 class Shoppinglist(object):
-    """a class to manage a shopping list"""
-
-    #a list to hold all shopping lists
-    shoppinglists = []
+   
+    
 
     def __init__(self ):
-        """ initialize a dictionary to hold the details of a particular list"""
+        
         self.listsdict = {}
 
-    def newlist(self, listname, owner, listid):
-        """ A method to create a new list"""
-        for shoplist in self.shoppinglists:
-            if shoplist['listname'] == listname and shoplist['owner'] == owner:
-                return 'list exists'
-        # create the list if there is no list with the provided details
+    def newlist(self, name, description, owner):
+        """ a method to add a new list with the unique user"""
+        id = randint(1, 1000)
+        self.listsdict = {'name' : name, 'owner' : owner, 'description' : description, 'id' : id}
+        shoppinglists.append(dict(self.listsdict))
+        print(shoppinglists, "here")
+        res = 'success'
+        return res
+
+    def view_lists(self, owner):
+        """ a method to show lists belonging to a particular user"""
+        [items for items in a if d['name'] == 'pluto']
+        for items in shoppinglists:
+            if items['owner']== owner:
+                return items
         else:
-            self.listsdict = {'listname' : listname, 'owner' : owner, 'listid' : listid}
-            self.shoppinglists.append(self.listsdict)
+            return 'No Items'
+
+shoppingitems = []
+class ShoppingItems(object):
+   
+    
+
+    def __init__(self ):
+        
+        self.itemsdict = {}
+    """ a method to add items belonging to a specific shopping list"""
+    def new_item(self, name, description, list_id):
+            self.itemsdict = {'name' : name, 'list_id' : list_id, 'description' : description, 'item_id' : item_id}
+            shoppingitems.append(self.itemsdict)
+            print(shoppingitems, "here")
             res = 'success'
             return res
 
-    def view_lists(self, owner):
-        """ a method to show lists beonging to a particular user"""
-        #a new list containing names of lists belonging to  a certain user
-        user_shoplist = []
-        for s_list in self.shoppinglists:
-            if s_list['owner']== owner:
-                user_shoplist.append(s_list)
-        return user_shoplist
+    """ a method to show items belonging to a specific shopping list"""
+    def view_item(self, id):
+        for items in shoppingitems:
+            if items['id']== id:
+                return items
+        else:
+            return 'No Items'
+
+
+    """ a method to delete items in a particular list"""
+    def delete_item(self, item_id):
+        for items in shoppingitems:
+            if items['item_id']== item_id:
+                return items
+        else:
+            return 'No Items'
--- app/views.py
+++ app/views.py
@@ -3,39 +3,69 @@ from flask import Flask, url_for,render_template, request, flash, redirect, sess
 from app import app
 from models import User, Shoppinglist
 
+test = User()
+shop = Shoppinglist()
 @app.route('/', methods=['GET','POST'])
 def login():
-    if request.method=='POST':
-        username = request.form['username']
+    if request.method == 'POST':
+        email = request.form['email']
         password = request.form['password']
-        res = User.login(username=username, password=password)
-        print(res)
-        if res == True:
-            return redirect(url_for('dashboard'))
+        res = test.login(email, password)
+        if res is True:
+            session['logged_in'] = True
+            session['email'] = email
+            print session['email']
+            return redirect('/dashboard')
     else:
         
         return render_template('login.html')
 
-@app.route('/dashboard')
+@app.route('/dashboard', methods=['GET'])
 def dashboard_view():
-    """route to render dashboard page"""
-    return render_template('dashboard.html')
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        items = shop.view_lists(owner=session["email"])
+        print(items, "items")
+        return render_template('dashboard.html', data = items)
 
 @app.route('/register', methods=['GET','POST'])
 def register():
     if request.method=='POST':
         username = request.form['username']
         email = request.form['email']
-    # counter check if password is the same
-        password = request.form['password']
-        confpassword = request.form['confpassword']
-
+        password = request.form['pass']
+        confpassword = request.form['passc']
         if password != confpassword:
-            
             return redirect(url_for('register'))
-        res = User.signup(username, email, password)
+        res = test.signup(username, email, password)
         if res == "username or email exists":
             flash('username or email already taken', 'alert-danger')
-            return redirect(url_for('signup'))
+            return redirect(url_for('register'))
+        else:
+            return redirect(url_for('login'))
     else:    
         return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('login'))
+
+@app.route('/add_item', methods=['GET','POST'])
+def add_item():
+    error = None
+    if request.method=='POST':
+        name = request.form['name']
+        description = request.form['description']
+        owner = session["email"]
+        if not name.strip() and description.strip():
+            return redirect(url_for('add_item', error="No Data given"))
+        else:
+            result = shop.newlist(name, description, owner)
+            print(result, ">>>>>>>>")
+            if result == 'success':
+                return redirect(url_for('dashboard_view')) 
+        
+    else:    
+        return render_template('add-item.html')
